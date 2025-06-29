bb94e29c33e02aac1aab43f98160b4deadd13f62
김용민 <dydals3440@gmail.com>
2025-06-29 18:51:08 +0900

    chore: initialize java springboot

---
+++ src/main/java/com/yongcoding/YongcodingApplication.java
@@ -0,0 +1,12 @@
+package com.yongcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YongcodingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YongcodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yongcoding/YongcodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yongcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongcodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
