989a6c440f4ca77a213e7c3c008eb2b996d99842
김용민 <dydals3440@gmail.com>
2025-07-12 18:16:34 +0900

    docs: ascii docs 커스터마이제이션

--- src/test/java/com/yongcoding/api/controller/PostControllerDocTest.java
+++ src/test/java/com/yongcoding/api/controller/PostControllerDocTest.java
@@ -21,6 +21,7 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,11 +52,20 @@ public class PostControllerDocTest {
     @DisplayName("글 단건 조회 테스트")
     void test1() throws Exception {
         // given
-        Post post = Post.builder().title("123456789").content("bar").build();
+        Post post = Post.builder()
+            .title("123456789")
+            .content("bar")
+            .build();
         postRepository.save(post);
 
         // expected
-        ResultActions resultActions = this.mockMvc.perform(get("/posts/{postId}", 1L).accept(APPLICATION_JSON)).andDo(print()).andExpect(status().isOk()).andDo(document("index", pathParameters(parameterWithName("postId").description("게시글 ID")), responseFields(fieldWithPath("id").description("게시글 ID"), fieldWithPath("title").description("제목"), fieldWithPath("content").description("내용"))));
+        ResultActions resultActions =
+            this.mockMvc.perform(get("/posts/{postId}", 1L).accept(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-inquiry", pathParameters(parameterWithName("postId").description("게시글 ID")),
+                    responseFields(fieldWithPath("id").description("게시글 ID"), fieldWithPath("title").description("제목"),
+                        fieldWithPath("content").description("내용"))));
     }
 
     @Test
@@ -78,10 +88,12 @@ public class PostControllerDocTest {
             )
             .andDo(print())
             .andExpect(status().isOk())
-            .andDo(document("index",
+            .andDo(document("post-create",
                 requestFields(
-                    fieldWithPath("title").description("제목"),
+                    fieldWithPath("title").description("제목")
+                        .attributes(key("constraint").value("제목에 바보가 들어가면 안됩니다.")),
                     fieldWithPath("content").description("내용")
+                        .optional()
                 )
             ));
     }
